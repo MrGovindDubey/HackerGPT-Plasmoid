d57ced9a74e522117ada61761f4a40c64f1ff391
Govind Dubey <118271775+MrGovindDubey@users.noreply.github.com>
2024-04-09 21:54:19 +0530

    Update helper_functions.js

--- contents/ui/js/helper_functions.js
+++ contents/ui/js/helper_functions.js
@@ -1,62 +1,73 @@
-document.userScripts={saveData:{},config:{}};
+// Polyfill for Object.hasOwn
+if (!Object.hasOwn) {
+    Object.hasOwn = function(obj, prop) {
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+    };
+}
+
+document.userScripts = { saveData: {}, config: {} };
 
 document.userScripts.getMainInput = function() {
-		return document.querySelector('textarea.m-0');
+    return document.querySelector('textarea.m-0');
 }
 
 document.userScripts.getSendButton = function() {
-		return document.querySelector('textarea.m-0 + button.p-1');
+    const sendButton = document.querySelector('textarea.m-0 + button.p-1');
+    if (sendButton) {
+        return sendButton;
+    } else {
+        console.error("Send button not found");
+        return null;
+    }
 }
 
-
-document.userScripts.setInputFocus = function () {
-	let inputElement = document.userScripts.getMainInput();
-	if(inputElement) {
-		inputElement.focus();
-	}
-	console.log('setInputFocus');
+document.userScripts.setInputFocus = function() {
+    let inputElement = document.userScripts.getMainInput();
+    if (inputElement) {
+        inputElement.focus();
+    }
+    console.log('setInputFocus');
 }
 
+document.userScripts.setSendOnEnter = function() {
+    let inputElement = document.userScripts.getMainInput();
+    let sendButton = document.userScripts.getSendButton();
 
-document.userScripts.setSendOnEnter = function () {
-	let inputElement =  document.userScripts.getMainInput();
-	if(inputElement && !document.userScripts.saveData.oldOnPress) {
-		document.userScripts.saveData.oldOnPress = inputElement.onkeypress;
-		inputElement.onkeypress = function(e) {
-			if(	e.keyCode == 13  && 
-				!e.shiftKey && !e.ctrlKey && 
-				document.userScripts.config.sendOnEnter) {
-					document.userScripts.getSendButton().click();
-					return false;
-			}
-		}
-	}
-	console.log('setSendOnEnter');
+    if (inputElement && sendButton && !document.userScripts.saveData.oldOnPress) {
+        document.userScripts.saveData.oldOnPress = inputElement.onkeypress;
+        inputElement.onkeypress = function(e) {
+            if (e.keyCode === 13 && !e.shiftKey && !e.ctrlKey && document.userScripts.config.sendOnEnter) {
+                sendButton.click();
+                return false;
+            }
+        }
+    }
+    console.log('setSendOnEnter');
 }
 
-document.userScripts.setTheme = function (theme) {
-	if( document.userScripts.config && document.userScripts.config.matchTheme && theme ) {
-		localStorage.setItem('theme', theme);
-		console.log('setTheme');
-	}
+document.userScripts.setTheme = function(theme) {
+    if (document.userScripts.config && document.userScripts.config.matchTheme && theme) {
+        localStorage.setItem('theme', theme);
+        console.log('setTheme');
+    }
 }
 
-document.userScripts.getTheme = function () {	
-	return localStorage.getItem('theme');
+document.userScripts.getTheme = function() {
+    return localStorage.getItem('theme');
 }
 
-document.userScripts.removeSendOnEnter = function () {
-	let inputElement =  document.userScripts.getMainInput();
-	if(inputElement) {
-		inputElement.onkeypress = document.userScripts.saveData.oldOnPress;
-		document.userScripts.saveData.oldOnPress = null;
-	}
-	console.log('removeSendOnEnter');
+document.userScripts.removeSendOnEnter = function() {
+    let inputElement = document.userScripts.getMainInput();
+    if (inputElement) {
+        inputElement.onkeypress = document.userScripts.saveData.oldOnPress;
+        document.userScripts.saveData.oldOnPress = null;
+    }
+    console.log('removeSendOnEnter');
 }
 
-document.userScripts.setConfig = function (configuration) {
-	document.userScripts.config = configuration;
-	console.log('setConfig : ' + JSON.stringify(configuration));
+document.userScripts.setConfig = function(configuration) {
+    document.userScripts.config = configuration;
+    console.log('setConfig : ' + JSON.stringify(configuration));
 }
 
 console.log('Helper Functions loaded');
